2bb1a4fd1ef4dca66f3acc036e28fa465af089d4
normanqt2005 <quocthaiarct@gmail.com>
2026-05-30 08:33:26 +0700

    push first

---
+++ src/main/java/com/norman/swp391/JournalTrendBeApplication.java
@@ -0,0 +1,13 @@
+package com.norman.swp391;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalTrendBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JournalTrendBeApplication.class, args);
+    }
+
+}
